aaaa3fd1a943d6a2006361b1b5d86e2d18342880
terrorblade <terrorblade@e2d0575d-2f82-4250-b8ec-0d5e7f3c5cc0>
2011-09-09 17:38:11 +0000

    u9042hjoi4wyhetiokshfrti8yufopdseithjr;ghzdrjgdproxygtoierjirt5yujpo5teryge5iyer59tygedrpomtgpodfryc09tuyjrteothgfwe78trywe3iortygwe879yrth4weuo9tfwe789iht89we4rgtf879siwngsdtiuhtsegtrhiuowesbgfuisehnglkjsdgtfhsnvcyuiweghnfowegfrtiujkwehjnruiweklsweuiyfgbwen,kgfthrfgbejnmroiweugt rhfawesjtgeyhgtoierygtf8dzxhjgpoithdf8ufehwjnrtiopr78fwer4hyuierdfsgtuerhjtioerwuhgy8ihjdfgthsdkltgnsdufgtcyewopjtyh54er7896ty5ohgt87wer4ythj534epiorygt87er4w9yu6tu4wer8976 b34e8rty4w er9iothnjr4ewioutfg ueriwothew87rgty5erio5 y74e8tgh4i5er tg78f 78
    git-svn-id: https://testsrhoh3aga.myversioncontrol.com/subversion/eahoehaaa2245/trunk@66 e2d0575d-2f82-4250-b8ec-0d5e7f3c5cc0

--- src/arcemu-world/Level3.cpp
+++ src/arcemu-world/Level3.cpp
@@ -4284,5 +4284,6 @@ bool ChatHandler::HandleNPCCastCommand( const char *args, WorldSession *m_sessio
 bool ChatHandler::HandleReloadSpellCoefCommand(const char *args, WorldSession *m_session)
 {
 	objmgr.ReloadSpellCoef();
-	this->GreenSystemMessage(m_session, "Reloaded spell coef");
+	GreenSystemMessage(m_session, "Reloaded spell coef");
+	return true;
 }
