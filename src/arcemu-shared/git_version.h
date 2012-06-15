bc3ec46beeecc5030b9aae09e6483dba35a17bcb
terrorblade <terrorblade@e2d0575d-2f82-4250-b8ec-0d5e7f3c5cc0>
2012-06-15 15:48:39 +0000

    git-svn-id: https://testsrhoh3aga.myversioncontrol.com/subversion/eahoehaaa2245/trunk@357 e2d0575d-2f82-4250-b8ec-0d5e7f3c5cc0

---
+++ src/arcemu-shared/git_version.h
@@ -0,0 +1,11 @@
+#ifndef GIT_VERSION_HPP
+#define GIT_VERSION_HPP
+
+#define BUILD_TAG "Pewpew"
+#define BUILD_HASH 0xe13fd4b
+#define COMMIT_TIMESTAMP 1321621262
+#define BUILD_HASH_STR "wubwub"
+#define BUILD_USER_STR "chaotic"
+#define BUILD_HOST_STR "Chaotic"
+
+#endif
