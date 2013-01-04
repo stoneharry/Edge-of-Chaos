16deac1e668155b66eaff5454facfe746180413a
admin <admin@e2d0575d-2f82-4250-b8ec-0d5e7f3c5cc0>
2013-01-04 17:10:44 +0000

    Look at this please terror. :G
    git-svn-id: https://testsrhoh3aga.myversioncontrol.com/subversion/eahoehaaa2245/trunk@407 e2d0575d-2f82-4250-b8ec-0d5e7f3c5cc0

--- src/arcemu-world/PlayerPacketWrapper.cpp
+++ src/arcemu-world/PlayerPacketWrapper.cpp
@@ -508,7 +508,13 @@ void Player::SendLoot(uint64 guid, uint8 loot_type, uint32 mapid)
 
 				if(iter->item.itemproto)
 				{
-					iter->roll = new LootRoll(60000, (m_Group != NULL ? m_Group->MemberCount() : 1),  guid, x, itemProto->ItemId, factor, uint32(ipid), GetMapMgr());
+					int numGroupActive = 1;
+					if (m_Group != NULL)
+					{
+						m_Group->GetMembersCount();
+						// Here we need to determine if members are actually online or not... If not online, reduce count
+					}
+					iter->roll = new LootRoll(60000, numGroupActive, guid, x, itemProto->ItemId, factor, uint32(ipid), GetMapMgr());
 
 					data2.Initialize(SMSG_LOOT_START_ROLL);
 					data2 << guid;
