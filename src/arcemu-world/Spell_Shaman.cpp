cf58aef66cbd8c6310beffebbc1e98a51cf54876
admin <admin@e2d0575d-2f82-4250-b8ec-0d5e7f3c5cc0>
2013-07-27 19:33:58 +0000

    Only fixed half the spell, doh!
    git-svn-id: https://testsrhoh3aga.myversioncontrol.com/subversion/eahoehaaa2245/trunk@548 e2d0575d-2f82-4250-b8ec-0d5e7f3c5cc0

--- src/arcemu-world/Spell_Shaman.cpp
+++ src/arcemu-world/Spell_Shaman.cpp
@@ -42,7 +42,7 @@ class FireNova : public Spell
 					{
 						totem = u_caster->summonhandler.GetSummonWithEntry(totem_ids[i]); // fire totem rank 1
 						if (totem != NULL)
-							break;
+							return result;
 					}
 					if(totem == NULL)
 					{
@@ -56,7 +56,14 @@ class FireNova : public Spell
 		}
 		void SpellEffectDummy(uint32 i)
 		{
-			Unit * totem = u_caster->summonhandler.GetSummonWithEntry(2523); // fire totem rank 1
+			int totem_ids[3] = {3903, 3902, 2523};
+			Unit * totem;
+			for (int i = 0; i < 3; i++)
+			{
+				totem = u_caster->summonhandler.GetSummonWithEntry(totem_ids[i]); // fire totem rank 1
+				if (totem != NULL)
+					break;
+			}
 			if(totem == NULL)
 			{
 				SendCustomError(SPELL_CUSTOM_ERROR_MUST_HAVE_FIRE_TOTEM);
