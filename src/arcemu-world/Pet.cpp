23046ed8f1f32ec9cb007e417a0228bf5de065bc
Harry <stoneharryjames@gmail.com>
2013-08-14 14:49:24 +0100

    Some work no pet AI - if attack untargetable/unattackable/friendly reset target list.

--- src/arcemu-world/Pet.cpp
+++ src/arcemu-world/Pet.cpp
@@ -1871,6 +1871,11 @@ void Pet::DealDamage(Unit* pVictim, uint32 damage, uint32 targetEvent, uint32 un
 		return;
 	if(pVictim->IsSpiritHealer())
 		return;
+	if (pVictim->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_SELECTABLE) || pVictim->HasFlag(UNIT_FIELD_FLAGS, UNIT_FLAG_NOT_ATTACKABLE_2) || isFriendly(this, pVictim))
+	{
+		WipeTargetList();
+		return;
+	}
 
 	if(pVictim != this)
 		CombatStatus.OnDamageDealt(pVictim);
